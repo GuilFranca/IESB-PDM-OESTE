42deaf9c63a0f46812ddbe250855247d2721139d
Guilherme França <guilfranca27@gmail.com>
2026-08-17 21:29:45 -0300

    Aula 17/08/26

--- praticas/pratica03/minhas-tarefas/App.js
+++ praticas/pratica03/minhas-tarefas/App.js
@@ -1,29 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { titulo } from './util';
-import titulo_padrao from "./util";
+import { TextInput, StyleSheet, Text, View, Button } from 'react-native';
+import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from "./mensagens";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{titulo}</Text>
-      <Text style={{margin: 20}} >{titulo_padrao}</Text>
-      <Button title="Clique Aqui"/>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+
+      <View style={{ flexDirection: "row", justifyContent: "space-between", flex: 1 }}>
+        <View style={{ width: "65%" }}>
+          <TextInput style={styles.inputText} placeholder={rotulo_input_meta} />
+        </View>
+
+        <View style={{ width: "30%" }}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    padding: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column"
   },
-
-  text: {
-    color: "red",
-    fontSize: 26,
+  inputText: {
+    borderColor: "#cccccc",
+    borderWidth: 1,
+  },
+  metaContainer: {
+    flex: 1,
   }
 });
