ec25e83fbb096f23e365d5e15501bb5cde98b9c7
Guilherme França <guilfranca27@gmail.com>
2026-08-18 21:40:32 -0300

    Aula 18/08/26

--- praticas/pratica03/minhas-tarefas/App.js
+++ praticas/pratica03/minhas-tarefas/App.js
@@ -1,22 +1,36 @@
 import { TextInput, StyleSheet, Text, View, Button } from 'react-native';
 import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from "./mensagens";
+import { useState } from 'react';
 
 export default function App() {
+
+  const [inputMetaText, setInputMetaText] = useState("");
+  const [metas, setMetas] = useState([]);
+
+  function metaInputHandler(inputText){
+    setInputMetaText(inputText)
+  }
+
+  function adicionarMetaHandler() {
+    setMetas([...metas, inputMetaText])
+  }
+
   return (
     <View style={styles.mainContainer}>
 
-      <View style={{ flexDirection: "row", justifyContent: "space-between", flex: 1 }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <View style={{ width: "65%" }}>
-          <TextInput style={styles.inputText} placeholder={rotulo_input_meta} />
+          <TextInput onChangeText={metaInputHandler} style={styles.inputText} placeholder={rotulo_input_meta} />
         </View>
 
         <View style={{ width: "30%" }}>
-          <Button title={rotulo_btn_cadastro_meta} />
+          <Button onPress={adicionarMetaHandler} title={rotulo_btn_cadastro_meta} />
         </View>
       </View>
 
       <View style={styles.metaContainer}>
         <Text>{rotulo_lista_metas}</Text>
+        {metas.map((meta, index) => <Text style={styles.item} key={index}>{meta}</Text>)}
       </View>
 
     </View>
@@ -35,5 +49,12 @@ const styles = StyleSheet.create({
   },
   metaContainer: {
     flex: 1,
+  },
+  item: {
+    padding: 10,
+    backgroundColor: "lightBlue",
+    borderRadius: 5,
+    marginTop: 5,
+    color: "white"
   }
 });
