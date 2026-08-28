60b8ef8436f791d301b014487ce4a87c40ee74a1
Guilherme França <guilfranca27@gmail.com>
2026-08-28 00:02:33 -0300

    feat: Insert de materias completo

--- praticas/pratica04/MeuDiarioAcademico/App.js
+++ praticas/pratica04/MeuDiarioAcademico/App.js
@@ -1,10 +1,28 @@
 import { StyleSheet, View } from 'react-native';
 import { MateriaInput } from './components/MateriaInput';
+import { MateriaList } from './components/MateriList';
+import { useState } from 'react';
 
 export default function App() {
+  const [inputMetaText, setInputMetaText] = useState("");
+  const [materias, setMaterias] = useState([]);
+
+  function aoDigitar(inputText) {
+    setInputMetaText(inputText)
+  }
+
+  function adicionarMateria() {
+    if (!inputMetaText) return
+    setMaterias([
+      ...materias,
+      inputMetaText
+    ]);
+  }
+
   return (
     <View style={styles.mainContainer}>
-      <MateriaInput />
+      <MateriaInput adicionarMateria={adicionarMateria} aoDigitar={aoDigitar}/>
+      <MateriaList materias={materias} />
     </View>
   );
 }
@@ -12,8 +30,9 @@ export default function App() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1d1c1b',
     flexDirection: "column",
-    padding: 25
+    padding: 25,
+    gap: 25
   },
 });
--- praticas/pratica04/MeuDiarioAcademico/components/MateriaInput.js
+++ praticas/pratica04/MeuDiarioAcademico/components/MateriaInput.js
@@ -1,36 +1,41 @@
-import { TextInput, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { TextInput, StyleSheet, TouchableOpacity, Text, View } from "react-native";
 import { btnText, inputText } from "../texts";
 
-export function MateriaInput() {
-    return(
-        <view style={styles.inputContainer}>
-            <TextInput style={styles.inputText} placeholder={inputText}/>
+export function MateriaInput({ adicionarMateria, aoDigitar }) {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput onChangeText={(text) => aoDigitar(text)} style={styles.inputText} placeholder={inputText} />
 
-            <TouchableOpacity style={styles.btn} onPress={() => {}}>
+            <TouchableOpacity style={styles.btn} onPress={() => adicionarMateria()}>
                 <Text style={styles.btnText}>{btnText}</Text>
             </TouchableOpacity>
-        </view>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
-        flexDirection: "row"
+        flexDirection: "row",
+        justifyContent: "space-between",
+        gap: 25
     },
     inputText: {
-        borderColor: "#cccccc",
-        borderWidth: 1,
-        padding: 5,
+        borderColor: "#750909",
+        borderWidth: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
         width: "50%",
+        backgroundColor: "#FFF",
+        borderRadius: 15,
     },
     btn: {
-        width: "50%",
+        width: "25%",
         backgroundColor: "#1b61e2",
         alignItems: "center",
+        justifyContent: "center",
         padding: 5
     },
     btnText: {
         color: "#FFF",
-    } 
+    }
 })
